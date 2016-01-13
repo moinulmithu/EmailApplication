50468b6d28ac4220cd8112ca590bb07a3c500d83
Moinul Islam <moinulmithu@hotmail.com>
2016-01-14 00:08:34 +0600

    2

---
+++ Demo/Models/Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Demo.Models
+{
+    public class Order
+    {
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+        public string ShippingAddress { get; set; }
+        public string EmailAddress { get; set; }
+    }
+}
---
+++ Demo/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Demo.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [StringLength(50)]
+        public string Name { get; set; }
+        [StringLength(1000)]
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        [StringLength(50)]
+        public string ImageName { get; set; }
+    }
+}
